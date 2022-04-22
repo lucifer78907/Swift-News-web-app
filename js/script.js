3b8e1482c9693b2ec8147a7b8c387095acba80d5
Rudra <chaudhary66667@gmail.com>
2022-04-22 10:59:58 +0530

    +benefits_section_start +arrow_img

--- js/script.js
+++ js/script.js
@@ -2,12 +2,15 @@ const heroBtnEle = document.querySelector(".header__link");
 const moonEle = document.querySelector(".moon");
 const sunEle = document.querySelector(".sunny");
 const bodyEle = document.querySelector("body");
+const imgEle = document.querySelector(".benefits__main-box--img");
 
+// Toggle Light and Dark theme
 heroBtnEle.addEventListener("click", function () {
   bodyEle.classList.toggle("light-theme");
   bodyEle.classList.toggle("dark-theme");
   sunEle.classList.toggle("hidden");
   moonEle.classList.toggle("hidden");
-});
 
-// btn ele animation
+  // Invert img color
+  imgEle.classList.toggle("invert_img");
+});
